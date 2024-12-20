5d87c88ead8c66866b7889962ca9ab343e726f52
Adeola1976 <51276682+Adeola1976@users.noreply.github.com>
2024-12-20 14:00:20 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
    //print hello world in the terminal window
-    System.out.printin("Hello world");
+    System.out.printIn("Hello world");
   }
 }
